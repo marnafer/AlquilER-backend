96b7451ed172acee3b4b11c9374693fa2340829e
marnafer <marianofernandez98@outlook.com>
2026-09-20 20:38:45 -0300

    Rollback cambio mensaje consulta repository

--- src/repositories/EloquentMensajeConsultaRepository.php
+++ src/repositories/EloquentMensajeConsultaRepository.php
@@ -2,65 +2,42 @@
 
 namespace App\Repositories;
 
-use App\Models\PropiedadImagen;
+use App\Models\MensajeConsulta;
 use Illuminate\Database\Eloquent\Collection;
 
-class EloquentPropiedadImagenRepository implements PropiedadImagenRepositoryInterface
+class EloquentMensajeConsultaRepository implements MensajeConsultaRepositoryInterface
 {
-    public function all(): Collection
+    public function create(array $data): MensajeConsulta
     {
-        return PropiedadImagen::query()
-            ->with('propiedad')
-            ->orderByDesc('id')
+        return MensajeConsulta::create($data);
+    }
+
+    public function findByConsultaId(int $consultaId): Collection
+    {
+        return MensajeConsulta::with('usuario')
+            ->where('consulta_id', $consultaId)
+            ->orderBy('fecha_mensaje', 'asc')
             ->get();
     }
 
-    public function findById(int $id): ?PropiedadImagen
+    public function findById(int $id): ?MensajeConsulta
     {
-        return PropiedadImagen::query()
-            ->with('propiedad')
-            ->whereKey($id)
-            ->first();
+        return MensajeConsulta::find($id);
     }
 
-    public function findByPropiedadId(int $propiedadId): Collection
+    public function update(int $id, array $data): bool
     {
-        return PropiedadImagen::query()
-            ->where('propiedad_id', $propiedadId)
-            ->orderByDesc('id')
-            ->get();
+        $mensaje = MensajeConsulta::find($id);
+
+        if (!$mensaje) {
+            return false;
+        }
+
+        return $mensaje->update($data);
     }
 
-    public function countByPropiedadId(int $propiedadId): int
+    public function delete(int $id): void
     {
-        return PropiedadImagen::query()
-            ->where('propiedad_id', $propiedadId)
-            ->count();
-    }
-
-    public function create(array $data): PropiedadImagen
-    {
-        return PropiedadImagen::create($data);
-    }
-
-    public function clearPrincipalByPropiedadId(int $propiedadId): void
-    {
-        PropiedadImagen::query()
-            ->where('propiedad_id', $propiedadId)
-            ->update([
-                'es_principal' => 0
-            ]);
-    }
-
-    public function setPrincipal(PropiedadImagen $imagen): void
-    {
-        $imagen->update([
-            'es_principal' => 1
-        ]);
-    }
-
-    public function delete(PropiedadImagen $imagen): void
-    {
-        $imagen->delete();
+        MensajeConsulta::destroy($id);
     }
 }
